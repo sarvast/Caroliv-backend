7b6fd6a3798eeabe9af35e077083b3843b24ebc7
sarvast <ss897@gmail.com>
2025-12-23 21:55:46 +0530

    fix: add x-admin-key to CORS allowed headers

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ const corsOptions = {
         : '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-key']
 };
 
 app.use(cors(corsOptions));
